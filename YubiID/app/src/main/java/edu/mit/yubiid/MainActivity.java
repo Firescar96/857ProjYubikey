770728afa05aea2dbd8c9d60ab997e562cf0de56
Firescar96 <nchinda2@gmail.com>
2015-05-08 20:29:16 -0400

    can't get text from yubikey anymore

--- YubiID/app/src/main/java/edu/mit/yubiid/MainActivity.java
+++ YubiID/app/src/main/java/edu/mit/yubiid/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.mit.yubiid;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -23,41 +30,224 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import edu.mit.yubiid.server.Contract.Exchanges;
-import edu.mit.yubiid.server.ExchangesDbHelper;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private static final String NEO_STORE = "NEO_STORE";
     private static final String YUBICO = "https://my.yubico.com/neo/";
+    public static final String MIME_TEXT_PLAIN = "text/plain";
     private static final String TAG = "MainActivity";
 
-    private ExchangesDbHelper dbHelper = new ExchangesDbHelper(this);
-    private SQLiteDatabase db = dbHelper.getReadableDatabase();
+    private static final int PUB_KEY = 1;
+    private static final int OTP = 2;
+    private int state = 0;
+
+    private String pubAddress;
+    private String otp;
+
+    //private ExchangesDbHelper dbHelper = new ExchangesDbHelper(this);
+    //private SQLiteDatabase db = dbHelper.getReadableDatabase();
     
     private static MainActivity context;
+    private NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
         context = this;
-        /*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }*/
 
-        if(getIntent() != null) {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(getIntent() != null)
+            handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (Exception e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    private void handleIntent(Intent intent) {
+        if(getIntent().getDataString() != null)
             Log.i(TAG, getIntent().getDataString());
-            ((EditText) findViewById(R.id.message)).setText(getIntent().getDataString().replace(YUBICO,""));
+        else
+            Log.i(TAG,getIntent().toString());
+        /*if(getIntent() != null) {
+            String pubAddress = getIntent().getDataString().split(":")[1];
+            String otp = getIntent().getDataString().split(":")[2];
+            ((EditText) findViewById(R.id.amount)).setText(otp);
+        }*/
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
         }
     }
 
+    /**
+     * @param activity The corresponding {@link Activity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+    /**
+     * Background task for reading the data. Do not block the UI thread while reading.
+     *
+     * @author Ralf Wondratschek
+     *
+     */
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
+
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e(TAG, "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String utf8 = "UTF-8";
+            String utf16 = "UTF-16";
+            String textEncoding = ((payload[0] & 128) == 0) ? utf8 : utf16;
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                if(state == PUB_KEY) {
+                    pubAddress = result.split(":")[PUB_KEY];
+                    ((EditText) findViewById(R.id.addresss)).setText(pubAddress);
+                }
+                if(state == OTP) {
+                    otp = result.split(":")[OTP];
+                    ((EditText) findViewById(R.id.addresss)).setText(otp);
+                }
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -105,7 +295,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             protected Object doInBackground(Object...arg) {
                 try {
-                    Connection con = Jsoup.connect(YUBICO+((EditText) findViewById(R.id.message)).getText());
+                    Connection con = Jsoup.connect(YUBICO+((EditText) findViewById(R.id.amount)).getText());
 
                     Document doc = con.get();
                     Elements info = doc.select("#info");
@@ -139,16 +329,23 @@ public class MainActivity extends ActionBarActivity {
                 ((TextView) context.findViewById(R.id.response)).setText("INVALID CODE");
         }
     };
+
+    public void setMode(View v) {
+        if(v.getId() == R.id.addressMode)
+            state = PUB_KEY;
+        else
+            state = OTP;
+    }
     
-   private void sendMoney(String senderyubikey, String publicyubikey, int amount) {
+   /* private void sendMoney(String senderyubikey, String publicyubikey, int amount) {
 	   ContentValues values = new ContentValues();
 
 	   values.put(Exchanges.COLUMN_NAME_SENDER_YUBIKEY, senderyubikey);
 	   values.put(Exchanges.COLUMN_NAME_PUBLIC_YUBIKEY, publicyubikey);
 	   values.put(Exchanges.COLUMN_NAME_AMOUNT, amount);
-	   
-	   int count = db.update(Exchanges.TABLE_NAME,values,null,null);
-			   
+
+       int count = db.update(Exchanges.TABLE_NAME,values,null,null);
+
    }
    
    private  void receiveMoney(String senderyubikey, String publicyubikey, int amount) {
@@ -160,6 +357,6 @@ public class MainActivity extends ActionBarActivity {
 	   
 	   int count = db.update(Exchanges.TABLE_NAME,values,null,null);
 			   
-   }
+   }*/
 
 }
